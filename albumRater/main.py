ebc1a992db1711d8bcc6926ee3863c9d02513324
jxshy18x <josh.coleman1807@gmail.com>
2026-05-10 18:14:56 +0100

    added delete function

--- albumRater/main.py
+++ albumRater/main.py
@@ -40,9 +40,6 @@ def addAlbum():
     print(json.dumps(ratedAlbums, indent=4))
     print(f"'{albumName}' by {artistName} saved!")
 
-
-
-
 def saveAlbums(albums):
     with open("albumRater/albums.json", "w") as f:
         json.dump(albums, f, indent=4)
@@ -59,6 +56,43 @@ def printAlbums():
                 print(f"    - {song}")
     print()
 
+def editAlbum():
+    ratedAlbums = loadAlbums()
+
+def deleteAlbum():
+    print("\n--Make sure to spell your artist correctly--\n")
+    
+    #Checking artist name exists
+    artistName = input("Enter your artist:")
+    if artistName not in ratedAlbums:
+        print(f"Artist {artistName}, not found in file.")
+        return
+    
+    #Checking album exists
+    albumName = input("Enter album name:")
+    if albumName not in ratedAlbums[artistName]:
+        print(f"{albumName} by {artistName} not found in file.")
+        return
+    
+    #Ask user to confirm before deletion
+    userConfirm = input(f"Are you sure you would like to delete {albumName} by {artistName}? (y/n)")
+    if userConfirm.lower() != "y":
+        print("Deleting process stopped")
+        return
+    
+    del ratedAlbums[artistName][albumName]
+
+    #if artist has no albums left, remove the artist
+    if len(ratedAlbums[artistName]) == 0:
+        del ratedAlbums[artistName]
+        print(f"Artist: {artistName}, removed due to no logged albums.")
+
+    saveAlbums(ratedAlbums)
+    
+
+
+def guessSong():
+    print("")
 
 
 # Main Code:
@@ -72,8 +106,17 @@ What features would you like to use?
   2: View albums
   3: Edit an album
   4: Delete an album
+  5: Play the guessing game
 """))
 
+if userChoice == 1:
+    addAlbum()
+elif userChoice == 2:
+    printAlbums()
+elif userChoice == 3:
+    editAlbum()
+elif userChoice == 4:
+    deleteAlbum()
 
 print("\n-------SPACER-------\n")
 
